ec9fd5f3789ada63bb3cb9228d6fa86a9e7a4014
Nageshjagtap <nageshjagtap063@gmail.com>
2025-03-20 12:00:00 +0530

    add the home component

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,51 +1,22 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchData } from './../src/assets/Component/apiSlice';
-
-const API_OPTIONS = {
-  'Pokémon API': 'https://pokeapi.co/api/v2/pokemon/ditto',
-  'GitHub Users': 'https://api.github.com/users/octocat',
-  'OpenWeather API': 'https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&current_weather=true',
-};
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './assets/Component/Navbar';
+import Home from './assets/Component/Home';
+import About from './assets/Component/About';
+import Api from "./assets/Component/Api";
+import Contact from './assets/Component/Contact';
 
 const App = () => {
-  const [selectedAPI, setSelectedAPI] = useState(Object.keys(API_OPTIONS)[0]);
-  const dispatch = useDispatch();
-  const { data, loading, error } = useSelector((state) => state.api);
-
-  const handleFetch = () => {
-    dispatch(fetchData(API_OPTIONS[selectedAPI]));
-  };
-
   return (
-    <div className="container">
-      <h1>API Explorer</h1>
-
-      <select value={selectedAPI} onChange={(e) => setSelectedAPI(e.target.value)}>
-        {Object.keys(API_OPTIONS).map((api) => (
-          <option key={api} value={api}>
-            {api}
-          </option>
-        ))}
-      </select>
-
-      <button onClick={handleFetch} disabled={loading}>
-        {loading ? 'Fetching...' : 'Fetch Data'}
-      </button>
-
-      {loading && <p>Loading...</p>}
-      {error && (
-        <p style={{ color: 'red' }}>
-          Error: {error} <button onClick={handleFetch}>Retry</button>
-        </p>
-      )}
-
-      {data && (
-        <pre style={{ textAlign: 'left', background: '#eee', padding: '10px' }}>
-          {JSON.stringify(data, null, 2)}
-        </pre>
-      )}
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+       <Route path="/apis" element={<Api />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 };
 
